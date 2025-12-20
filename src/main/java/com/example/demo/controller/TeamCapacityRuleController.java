b42a7514173398f25468a20c05db8e137aee99c6
bala-amypo <bala@amypo.in>
2025-12-20 05:25:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/TeamCapacityRuleController.java
+++ src/main/java/com/example/demo/controller/TeamCapacityRuleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/capacity-config")
-public class TeamCapacityConfigController {
+public class TeamCapacityRuleController {
 
     private final TeamCapacityRuleService ruleService;
 
