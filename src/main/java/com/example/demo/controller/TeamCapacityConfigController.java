d8adafdc3a910bb5555f16c69729625d0653c389
bala-amypo <bala@amypo.in>
2025-12-20 06:46:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
+++ src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/capacity-rules")
-public class TeamCapacityRuleController {
+public class TeamCapacityConfigController {
 
     private final TeamCapacityRuleService service;
 
--- src/main/java/com/example/demo/service/TeamCapacityCapacityService.java
+++ src/main/java/com/example/demo/service/TeamCapacityCapacityService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.TeamCapacityConfig;
 import java.util.List;
 
-public interface TeamCapacityRuleService {
+public interface TeamCapacityConfigService {
     TeamCapacityConfig save(TeamCapacityConfig config);
     List<TeamCapacityConfig> getAll();
 }
