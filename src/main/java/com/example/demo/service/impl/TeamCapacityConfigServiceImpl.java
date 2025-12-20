db3fa5425c084905753a9024c8d49beedbb921d2
bala-amypo <bala@amypo.in>
2025-12-20 06:49:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TeamCapacityConfigServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TeamCapacityConfigServiceImpl.java
@@ -2,17 +2,17 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.TeamCapacityConfig;
 import com.example.demo.repository.TeamCapacityConfigRepository;
-import com.example.demo.service.TeamCapacityRuleService;
+import com.example.demo.service.TeamCapacityConfigService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TeamCapacityRuleServiceImpl implements TeamCapacityRuleService {
+public class TeamCapacityConfigServiceImpl implements TeamCapacityConfigService {
 
     private final TeamCapacityConfigRepository repository;
 
-    public TeamCapacityRuleServiceImpl(TeamCapacityConfigRepository repository) {
+    public TeamCapacityConfigServiceImpl(TeamCapacityConfigRepository repository) {
         this.repository = repository;
     }
 
